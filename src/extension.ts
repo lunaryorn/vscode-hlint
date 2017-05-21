d51973db90d73ea7ec1f276cfd18cb9b2266e878
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-05-21 21:11:23 +0200

    Implement simple hlint linting

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,121 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Copyright (C) 2017  Sebastian Wiesner <swiesner@lunaryorn.com>
+//
+// This file is part of vscode-hlint.
+//
+// vscode-hlint is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// vscode-hlint is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with vscode-hlint.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 import * as vscode from 'vscode';
+import { execFile } from 'child_process';
+import { existsSync } from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+type HlintSeverity = 'Ignore' | 'Suggestion' | 'Warning' | 'Error';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hlint" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+interface IHlintMessage {
+    /** The module this message appeared in. */
+    readonly module: string;
+    /** The declaration the message appeared in. */
+    readonly decl: string;
+    /** The severity of this message. */
+    readonly severity: HlintSeverity;
+    /** The name of the hint that triggered this message. */
+    readonly hint: string
+    /** The path to the file the message appeared in. */
+    readonly file: string;
+    /** The line the message started. */
+    readonly startLine: number;
+    /** The column the message started. */
+    readonly startColumn: number
+    /** The line the message ended. */
+    readonly endLine: number;
+    /** The column the message ended. */
+    readonly endColumn: number;
+    /** The faulty Haskell expression that caused the message. */
+    readonly from: string;
+    /** The suggested replacement. */
+    readonly to: string;
+    /** Additional notes? */
+    readonly note: string[];
+    /** Internal representation of suggested refactoring? */
+    readonly refactorings: string;
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+const toDiagnosticSeverity = (hlintSeverity: HlintSeverity): vscode.DiagnosticSeverity => {
+    switch (hlintSeverity) {
+        case 'Suggestion':
+            return vscode.DiagnosticSeverity.Hint
+        case 'Warning':
+            return vscode.DiagnosticSeverity.Warning;
+        case 'Error':
+            return vscode.DiagnosticSeverity.Error;
+        default:
+            return vscode.DiagnosticSeverity.Information
+    }
+};
+
+const toDiagnostic = (hlintMessage: IHlintMessage): vscode.Diagnostic => {
+    const range = new vscode.Range(
+        hlintMessage.startLine - 1,
+        hlintMessage.startColumn - 1,
+        hlintMessage.endLine - 1,
+        hlintMessage.endColumn - 1);
+    const message = hlintMessage.to ?
+        `${hlintMessage.hint}. Replace with ${hlintMessage.to}` :
+        hlintMessage.hint;
+    const severity = toDiagnosticSeverity(hlintMessage.severity);
+    return new vscode.Diagnostic(range, message, severity);
+};
+
+
+/**
+ * Lint a single text document.
+ *
+ * @param document The text document to lint
+ */
+const lintDocument = (diagnosticsCollection: vscode.DiagnosticCollection) => (document: vscode.TextDocument): void => {
+    if (document.isDirty || (!existsSync(document.fileName))) {
+        // Bail out if the document isn't saved or doesn't exist no disk
+        return;
+    }
+    const options = { cwd: vscode.workspace.rootPath || process.cwd() };
+    execFile('hlint', ['--json', document.fileName], options, (error, stdout, stderr) => {
+        if (error && (<any>error).errno) {
+            // Check if an errno property exists on the error.  If so it's a
+            // system error which we should tell the user about, otherwise it's
+            // perhaps just the non-zero exit code of hlint when it found something
+            vscode.window.showErrorMessage(`Failed to run hlint: ${error.message}`);
+        } else if (stderr.length > 0) {
+            vscode.window.showErrorMessage(`hslint failed: ${stderr}`);
+        } else {
+            const messages = JSON.parse(stdout) as IHlintMessage[];
+            const diagnostics = messages
+                .filter(msg => msg.file === document.fileName)
+                .map(toDiagnostic);
+            diagnosticsCollection.set(document.uri, diagnostics);
+        }
+    });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    const diagnostics = vscode.languages.createDiagnosticCollection('hlint2');
+    context.subscriptions.push(diagnostics);
+
+    // Start linting documents when they are saved or freshly opened
+    vscode.workspace.onDidSaveTextDocument(lintDocument(diagnostics), null, context.subscriptions);
+    vscode.workspace.onDidOpenTextDocument(lintDocument(diagnostics), null, context.subscriptions);
+    // Remove a document from the diagnostics collection when it's closed
+    vscode.workspace.onDidCloseTextDocument(document => {
+        diagnostics.delete(document.uri);
+    }, null, context.subscriptions);
 }
