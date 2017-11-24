f6b95eaf39d2c308da0f0a14f85b67692da30f65
Sebastian Wiesner <sebastian@swsnr.de>
2017-11-24 19:02:00 +0100

    Update my mail address

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-//    Copyright 2017 Sebastian Wiesner <swiesner@lunaryorn.com>
+//    Copyright 2017 Sebastian Wiesner <sebastian@swsnr.de>
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
